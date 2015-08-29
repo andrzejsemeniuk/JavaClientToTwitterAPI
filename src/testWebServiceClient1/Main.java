0de96774d496b918d92e211c3c57b4eb755b3dbe
botanik <as180980-dev@yahoo.com>
2015-08-28 20:44:16 -0500

    -ditto

--- src/testWebServiceClient1/Main.java
+++ src/testWebServiceClient1/Main.java
@@ -4,26 +4,18 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
  
-//import org.glassfish.grizzly.http.server.HttpServer;
- 
-//...
- 
 public class Main {
  
-	static private final String BASE_URI = "";
 	
-    static private WebTarget target;
- 
     static public void main(String[] args) 
     {
-        Client c = ClientBuilder.newClient();
-        
-        String parameters = "parameters";
-        
-        target = c.target(Main.BASE_URI+"?"+parameters);
- 
-        String responseMsg = target.path("myresource").request().get(String.class);
-        
-        System.out.println("response="+responseMsg);
+    	// process:
+    	//  1 authenticate
+    	//  2 call api
+    	
+    	TwitterClient client = new TwitterClient();
+    	
+    	client.search("Austin",3);
+    	
     }
 }
